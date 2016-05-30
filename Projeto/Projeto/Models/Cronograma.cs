1ff35137b42383b2477d127a20e98e41f0f3289d
unknown <thaynan.nunes@ccc.ufcg.edu.br>
2016-05-30 12:17:59 -0700

    Adicao classe Cronograma

---
+++ Projeto/Projeto/Models/Cronograma.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto.Models
+{
+    public class Cronograma
+    {
+
+        public Dictionary<int, string> segunda;
+        public Dictionary<int, string> terca;
+        public Dictionary<int, string> quarta;
+        public Dictionary<int, string> quinta;
+        public Dictionary<int, string> sexta;
+        public Dictionary<int, string> sabado;
+        public Dictionary<int, string> domingo;
+        public List<Pessoa> pessoasCadastradas;
+
+        public Cronograma()
+        {
+            segunda = new Dictionary<int, string>();
+            terca = new Dictionary<int, string>();
+            quarta = new Dictionary<int, string>();
+            quinta = new Dictionary<int, string>();
+            sexta = new Dictionary<int, string>();
+            sabado = new Dictionary<int, string>();
+            domingo = new Dictionary<int, string>();
+            pessoasCadastradas = new List<Pessoa>();
+        }
+
+        private void carrega()
+        {
+            //Carregar o banco de dados ou arquivo para salvar na lista de pessoas
+        }
+
+    }
+}
--- Projeto/Projeto/Models/Pessoa.cs
+++ Projeto/Projeto/Models/Pessoa.cs
@@ -1,11 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace Projeto.Models
 {
     public class Pessoa
     {
+        private string Nome { get; set; }
+        private Dictionary<string, List<int>> horarios;
+
+        public Pessoa(string nomePessoa)
+        {
+            Nome = nomePessoa;
+            horarios = new Dictionary<string, List<int>>()
+            {
+                {"segunda", new List<int>()},
+                {"terca", new List<int>()},
+                {"quarta", new List<int>()},
+                {"quinta", new List<int>()},
+                {"sexta", new List<int>()},
+                {"sabado", new List<int>()},
+                {"domingo", new List<int>()}
+            };
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public void addHorario(string dia, int hora)
+        {
+            if (horarios.ContainsKey(dia))
+            {
+                List<int> lista = horarios[dia];
+                lista.Add(hora);
+                horarios.Add(dia, lista);
+            }
+            
+            /*foreach(var item in horarios)
+            {
+                if (item.Key.Equals(dia))
+                    {
+                        List<int> lista = item.Value;
+                        if(lista == null)
+                        {
+                            lista = new List<int>;
+                        }
+                        lista.Add(hora);
+                        horarios.Add(dia, lista);
+                    }
+            }*/
+                
+       }
     }
 }
